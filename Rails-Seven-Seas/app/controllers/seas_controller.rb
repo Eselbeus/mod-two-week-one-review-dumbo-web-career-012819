092b6443ae5cf0f5ed820602181ccd32d0ee489e
github username <github email address>
2019-02-22 18:20:28 -0500

    yes

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,5 +1,45 @@
+# require 'pry'
 class SeasController < ApplicationController
   #define your controller actions here
+  def index
+    @seas = Sea.all
+    render :index
+  end
+
+  def new
+    @sea = Sea.new
+    render :new
+  end
+
+  def create
+    # byebug
+    @sea = Sea.create(sea_params)
+    redirect_to :seas
+  end
+
+  def show
+    @sea = Sea.find(params[:id])
+    render :show
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params)
+    render :show
+  end
+
+  def destroy
+    @sea = Sea.find(params[:id])
+    @sea.destroy
+    redirect_to :seas
+  end
+
+
 
 
   private
